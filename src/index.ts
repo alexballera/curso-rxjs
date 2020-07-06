d811dc219fe55e4e58adaa4b4433586442680fc8
Alex Ballera <alexballera@gmail.com>
2020-07-06 11:38:19 -0300

    Subscripcion y unsubscripción

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import { Observable, Observer } from 'rxjs';
 
 /** 
- * Crear un observable
- * const obs$ = Observable.create()
+ * Crear observer
 */
 
 
 const observer: Observer<any> = {
-  next: value => console.log('siguiente (next):', value),
-  error: error => console.warn('error (obs):', error),
-  complete: () => console.info('Completado')
+  next: value => console.log('[next]:', value),
+  error: error => console.warn('[error]:', error),
+  complete: () => console.info('completado')
 }
+
+const intervalo$ = new Observable<number>(subs => {
+  let contador = 0
+  const intervalo = setInterval(() => {
+    contador ++;
+
+    subs.next(contador);
+    
+  }, 1000)
+
+  // Se ejecuta con el seTimeout unsubscribe y de esta manera 
+  // se destruye el setInterval y de desusbscribe
+  return () => {
+    clearInterval(intervalo)
+    console.log('Intérvalo destruído')
+  }
+})
+
+const subs1 = intervalo$.subscribe(num => console.log('Num:', num))
+
+// Se desubscribe al término de 3 seg o según la lógica de negocio
+setTimeout(() => {
+  subs1.unsubscribe()
+}, 3000);
