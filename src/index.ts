0578fe9f2efa9f3fb6821d968fb49ec18ea67732
Alex Ballera <alexballera@gmail.com>
2020-07-06 23:32:43 -0300

    Map

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-import { of, from } from 'rxjs';
+import {range, fromEvent} from 'rxjs'
+import { map} from 'rxjs/operators';
 /** 
- * of: toma argumentos y genera una secuencia
+ * map: extraer, transformar información
+ * https://rxjs-dev.firebaseapp.com/api/operators/map
  * from: crea secuencia obs, objeto iterable
+ * type <recibe, emite>
 */
+
+// range(1,5).pipe(
+//   map<number,string>(val => (val*10).toString())
+// )
+// .subscribe(console.log)
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup')
+
+const keyupCode$ = keyup$.pipe(
+  map(event => event.code)
+)
+
+keyupCode$.subscribe(code => console.log('map', code))
