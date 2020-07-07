f0bc19a3acc478457f37ced3e4ed77ce2e82a69f
Alex Ballera <alexballera@gmail.com>
2020-07-07 08:09:10 -0300

    Map pluck MapTo

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,3 @@ import { map, pluck, mapTo} from 'rxjs/operators';
  * https://rxjs-dev.firebaseapp.com/api/operators/mapTo
  * type <recibe, emite>
 */
-
-// range(1,5).pipe(
-//   map<number,string>(val => (val*10).toString())
-// )
-// .subscribe(console.log)
-
-const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup')
-
-const keyupCode$ = keyup$.pipe(
-  map(event => event.code)
-)
-
-// const keyupPluck$ = keyup$.pipe(
-//   pluck('key')
-// )
-
-const keyupPluck$ = keyup$.pipe(
-  pluck('target', 'baseURI')
-)
-
-const keyupMapTo$ = keyup$.pipe(
-  mapTo('Tecla presionada')
-)
-
-keyup$.subscribe(console.log)
-keyupCode$.subscribe(code => console.log('map', code))
-keyupPluck$.subscribe(code => console.log('pluck', code))
-keyupMapTo$.subscribe(code => console.log('mapTo', code))
