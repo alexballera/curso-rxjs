a9a845f248df3ce6e6903a5b080676e5b1130962
Alex Ballera <alexballera@gmail.com>
2020-07-07 08:59:43 -0300

    Filter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,47 @@
-import {range, fromEvent} from 'rxjs'
-import { map, pluck, mapTo} from 'rxjs/operators';
+import {range, from} from 'rxjs'
+import { filter, pluck } from 'rxjs/operators';
 /** 
- * map: extraer, transformar información
- * pluck: extrae un valor del objeto
- * mapTo: emite cualquier valor predeterminado
- * https://rxjs-dev.firebaseapp.com/api/operators/map
- * https://rxjs-dev.firebaseapp.com/api/operators/pluck
- * https://rxjs-dev.firebaseapp.com/api/operators/mapTo
- * type <recibe, emite>
+ * filter: operador que filtra item que al evaluarlo cumple
+ * con la condición
+ * https://rxjs-dev.firebaseapp.com/api/operators/filter
+ * parámetros: val, index
+ * retorna: booleano, thisArg 
 */
+// range(1,10).pipe(
+//   // Muestra impares
+//   filter(val => val % 2 === 1)
+// ).subscribe(console.log)
+
+range(10,20).pipe(
+  // Muestra impares
+  filter((val, i) => {
+    console.log('index', i)
+    return val % 2 === 1
+  })
+)//.subscribe(console.log)
+
+interface Personaje {
+  tipo: string;
+  nombre: string;
+}
+const personajes: Personaje[] = [
+  {
+    tipo: 'heroe',
+    nombre: 'Batman',
+  },
+  {
+    tipo: 'heroe',
+    nombre: 'Superman',
+  },
+  {
+    tipo: 'villano',
+    nombre: 'Dr Eggman',
+  },
+]
+
+const src$ = from(personajes)
+
+src$.pipe(
+  filter(val => val.tipo === 'heroe'),
+  pluck('nombre')
+).subscribe(console.log)
