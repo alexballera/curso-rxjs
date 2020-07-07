c00399acf3ea4360d9ab851bdc27a7517a3a797a
Alex Ballera <alexballera@gmail.com>
2020-07-06 23:40:24 -0300

    Pluck

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {range, fromEvent} from 'rxjs'
-import { map} from 'rxjs/operators';
+import { map, pluck} from 'rxjs/operators';
 /** 
  * map: extraer, transformar información
+ * pluck: extrae un valor del objeto
  * https://rxjs-dev.firebaseapp.com/api/operators/map
+ * https://rxjs-dev.firebaseapp.com/api/operators/pluck
  * from: crea secuencia obs, objeto iterable
  * type <recibe, emite>
 */
@@ -18,4 +20,14 @@ const keyupCode$ = keyup$.pipe(
   map(event => event.code)
 )
 
+// const keyupPluck$ = keyup$.pipe(
+//   pluck('key')
+// )
+
+const keyupPluck$ = keyup$.pipe(
+  pluck('target', 'baseURI')
+)
+
+keyup$.subscribe(console.log)
 keyupCode$.subscribe(code => console.log('map', code))
+keyupPluck$.subscribe(code => console.log('pluck', code))
