a6ec916ae96306495ffe0aa05f9414e9f43bfe19
Alex Ballera <alexballera@gmail.com>
2020-07-08 09:11:42 -0300

    Fetch Api 2

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,18 @@
 
 const url = 'https://api.github.com/users?per_page=5'
 
+// Para que se dispare el error hay que manehar el catch en las promesas
+const mensajeErrores = (response: Response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response
+}
+
 const fetchPromesa = fetch(url)
 
 fetchPromesa
+  .then(mensajeErrores)
   .then(resp => resp.json())
   .then(data => console.log('data:', data))
   .catch(err => console.warn('Error', err))
