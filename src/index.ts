0da3c44631756bf9b3c496f3e5e5c7acd9ac795c
Alex Ballera <alexballera@gmail.com>
2020-07-09 00:04:57 -0300

    MergeAll

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { fromEvent } from 'rxjs';
-import { debounceTime, map, pluck, mergeAll } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
 /** 
  * mergeAll: Convierte un Observable de orden superior en un
  * Observable de primer orden que entrega simultáneamente todos
@@ -8,24 +5,3 @@ import { ajax } from 'rxjs/ajax';
  * mergeAll<T>(concurrent: number = Number.POSITIVE_INFINITY): OperatorFunction<ObservableInput<T>, T>
  * https://rxjs-dev.firebaseapp.com/api/operators/mergeAll
 */
-
-// Referencias
-const body = document .querySelector('body')
-const textInput = document.createElement('input')
-const orderList = document.createElement('ol')
-body.append(textInput, orderList)
-
-// Streams
-const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup')
-
-input$.pipe(
-  debounceTime(500),
-  pluck('target', 'value'),
-  map(texto => ajax.getJSON(
-    `https://api.github.com/search/users?q=${texto}`
-  )),
-  mergeAll(),
-  pluck('items')
-).subscribe(resp => {
-  console.log(resp)
-})
